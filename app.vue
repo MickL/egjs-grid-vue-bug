e15cbadaf0a90c64127fb5e37101d7563c244b3d
Mick Lawitzke <info@lawitzke.de>
2025-04-10 16:03:44 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { MasonryGrid, JustifiedGrid, FrameGrid, PackingGrid } from "@egjs/vue-grid";
+</script>
+
+<template>
+  <masonry-grid
+      class="container"
+      :gap="10"
+      align="justify"
+  >
+    <div class="item">1</div>
+    <div class="item">2</div>
+    <div class="item">3</div>
+    <div class="item">4</div>
+    <div class="item">5</div>
+    <div class="item">6</div>
+    <div class="item">7</div>
+    <div class="item">8</div>
+    <div class="item">9</div>
+    <div class="item">10</div>
+  </masonry-grid>
+</template>
